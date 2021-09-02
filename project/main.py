dac588059d8fa6171e82ae4b56755c661a7dd008
harshpatel5940 <harshpatel5940>
2021-09-02 11:48:44 +0530

    Visual changes and cleaned the code

--- project/main.py
+++ project/main.py
@@ -1,29 +1,32 @@
 from modules import *
 
-print("xxxxxxxxxxxxxxxx DEVICE USAGE TRACKER (Stable) xxxxxxxxxxxxxxxx")
+print("xxxxxxxxxxxxxxxx DEVICE USAGE TRACKER xxxxxxxxxxxxxxxx\n\n")
 
 while True:
-    print("===================== MAIN APP MENU =====================")
-    ask1 = input("1) Login as user (normal member)\n2) Login as super user (admin)\n3) Register (signup)\n4) Close App\nWhat you want to do [1] [2] [3] [4] : ")
+    ask1 = input(app_menu())
     if ask1 == "1":
         id1 = login()
         while True:
-            print("===================== MAIN USER MENU =====================")
-            task = input("1)Show all apps\n2) Create new app tracking\n3) Add tracking data to an existing app\n4) Read tacking data of an app\n5) Logout User\nWhat you want to do [1] [2] [3] [4] : ")
+            task = input(user_menu())
             if task == "1":
                 apps_count(id1)
             elif task == "2":
-                app = input("Enter the name of the app you want to create")
+                app = input("Enter the name of the app you want to create : ")
                 app_data_create(id1, app)
             elif task == "3":
-                app = input("Enter the name of the app")
-                app_data_inserter(id1, app)
-            elif task == "4":
-                app = input("Enter the name of the app")
+                app = input("Enter the name of the app you want to read data: ")
                 app_data_read(id1, app)
+            elif task == "4":
+                app = input("Enter the name of the app you want to add data: ")
+                app_data_inserter(id1, app)
             elif task == "5":
-                print("Logging out....")
+                user_password_update(id1)
+            elif task == "6":
+                log_out()
                 break
+            else:
+                invalid_option()
+                pass
     elif ask1 == "2":
         tup = admin_login()
         id1 = tup[0]
@@ -31,8 +34,7 @@ while True:
 
         if power == "3":
             while True:
-                print("================= Authorised Owner Menu! ==============")
-                ask1 = input("1) add admin\n2) remove admin\n3) Show all users data\n4) update user password\n5) Show app tracked for the user\n6)logout\nWhat you want to do [1] [2] [3] [4] [5] : ")
+                ask1 = input(owner_menu())
                 if ask1 == "1":
                     add_admin()
                 elif ask1 == "2":
@@ -41,45 +43,49 @@ while True:
                     user_count()
                     show_all_users()
                 elif ask1 == "4":
-                    update_user_data()
+                    admin_password_update()
                 elif ask1 == "5":
+                    show_all_admins()
+                elif ask1 == "6":
                     try:
-                        id1 = int(input("Enter the user id you want to show apps"))
+                        id1 = int(input("Enter the user id you want to show apps : "))
                         apps_count(id1)
                     except ValueError:
                         print("Invalid id")
-                elif ask1 == "6":
+                elif ask1 == "7":
+                    log_out()
                     break
                 else:
-                    print("Invalid option")
+                    invalid_option()
                     pass
 
         elif power == "2":
             while True:
-                print("================= Authorised Admin Menu! ==============")
-                ask1 = input("1) Show all users data\n2) update user password\n3) Show all Apps for a user \n4)logout\nWhat you want to do [1] [2] [3] : ")
+                ask1 = input(admin_menu())
                 if ask1 == "1":
                     user_count()
                     show_all_users()
                 elif ask1 == "2":
-                    update_user_data()
+                    admin_password_update()
                 elif ask1 == "3":
                     try:
-                        id1 = int(input("Enter the user ID for which you want to show apps"))
+                        id1 = int(input("Enter the user ID for which you want to show apps : "))
                         apps_count(id1)
                     except ValueError:
                         print("Invalid id!!")
                 elif ask1 == "4":
+                    log_out()
                     break
                 else:
-                    print("Invalid option!!")
+                    invalid_option()
                     pass
         else:
-            print("invalid power!!")
+            invalid_option()
     elif ask1 == "3":
         sign_up()
     elif ask1 == "4":
+        closing()
         break
     else:
-        print("incorrect option!!")
+        invalid_option()
         pass
--- project/modules.py
+++ project/modules.py
@@ -1,24 +1,99 @@
 import csv
 from pandas import DataFrame as Df
 from os import listdir as ls
+from termcolor import colored, cprint  # @nishanth & @ suraj ** REQUIRE : pip install termcolor **
 
 user_file_path = "D:\Sindhi Model School (SMS)\HARSH\.PROGRAMS\School\project\data.csv"
 admin_file_path = "D:\Sindhi Model School (SMS)\HARSH\.PROGRAMS\School\project\data_admin.csv"
 app_data_path = "D:\Sindhi Model School (SMS)\HARSH\.PROGRAMS\School\project\data"
 
 
-def write_user(id11, name, password):
-    with open(user_file_path, "a", newline="") as f:
-        print("Data Is OK")
-        file = csv.writer(f)
-        user_row = [id11, name, password]
-        file.writerow(user_row)
-        print("New User Is Created!")
-        
+def color(text):
+    color_text = colored(text, 'cyan')
+    return color_text
+
+
+def app_menu():
+    x = f"""
+{color("===================== MAIN APP MENU =====================")}
+1) Login as user (normal member)
+2) Login as super user (admin)
+3) Register (signup)
+4) Close App
+What you want to do [1] [2] [3] [4] : """
+    return x
+
+
+def user_menu():
+    x = f"""
+{color("===================== Authorised User MENU =====================")}
+1) Show all apps
+2) Create new app tracking
+3) Add tracking data to an existing app
+4) Read tacking data of an app
+5) {colored("Update User Password", 'red')}
+6) Logout User
+What you want to do [1] [2] [3] [4] : """
+    return x
+
+
+def owner_menu():
+    x = f"""
+{color("===================== Authorised Owner Menu! =====================")}
+1) add admin
+2) remove admin
+3) Show all users data
+4) update user password
+5) Show All Admins
+6) Show app tracked for the user
+7) logout
+What you want to do [1] [2] [3] [4] [5] [6] [7]: """
+    return x
+
+
+def admin_menu():
+    x = f"""
+{color("===================== Authorised Admin Menu! =====================")}
+1) Show all users data
+2) update user password
+3) Show all Apps for a user 
+4)logout
+What you want to do [1] [2] [3] [4]: """
+    return x
+
+
+def invalid_option():
+    cprint("Invalid option", "red")
+
+
+def log_out():
+    cprint("Logging Out...", "cyan")
+
+
+def closing():
+    cprint("Closing...", "cyan")
+
+
+def file_exsist_warning():
+    x = colored("""
+WARNING!!
+
+-----The App Data For The Current User Has Already Been Created/Existing.
+-----Creating it Again Leads to Loss of Data 
+
+Do you want to continue? [y] or [n] : """, "red")
+    return x
 
 
 def show_all_users():
     file1 = open(user_file_path, 'r')
+    reader = csv.reader(file1)
+    print(Df(reader, ))
+    file1.close()
+
+
+def show_all_admins():
+    file1 = open(admin_file_path, 'r')
     reader = csv.reader(file1)
     print(Df(reader, ))
     file1.close()
@@ -32,7 +107,7 @@ def user_count():
         users += 1
 
     users -= 1
-    print("Number of Users Are ", users)
+    cprint(f" == > Number of Users Are {users}", "cyan")
 
 
 def apps_count(id1):
@@ -51,58 +126,65 @@ def apps_count(id1):
         name = app.split('.')
         app_list_final.append(name[0])
     if app_list_final == []:
-        print("User is Invalid or User Has No Apps Tracked")
+        cprint("User is Invalid or User Has No Apps Tracked", "red")
+
     else:
-        appcount = len(app_list_final)
-        print(f"Total Apps Tracked For User{id1} = {appcount}\n The Apps Are ---> {app_list_final}")
+        app_count = len(app_list_final)
+        print(f"Total Apps Tracked For User{id1} = {app_count}\n The Apps Are ---> {app_list_final}")
+
+
+def write_user(id11, name, password):
+    with open(user_file_path, "a", newline="") as f:
+        cprint("Data Is OK", 'green')
+        file = csv.writer(f)
+        user_row = [id11, name, password]
+        file.writerow(user_row)
 
 
 def app_data_create(id1, app_name):
     file_list = ls(app_data_path)
 
-    fname = f"User{id1}-{app_name}.csv"
+    file_name = f"User{id1}-{app_name}.csv"
 
-    if fname in file_list:
-        opt = input(
-            "WARNING!!\n-----The App Data For The Current User Has Already Been Created/Existing.\n-----Creating it Again Leads to Loss of Data\nDo you want to continue?[y]or[n] ")
-
+    if file_name in file_list:
+        opt = input(file_exsist_warning())
         if opt == 'y':
-            f = open(f"{app_data_path}/{fname}", 'w', newline="")
+            f = open(f"{app_data_path}/{file_name}", 'w', newline="")
             fw = csv.writer(f)
             column = ["Week Number", "Usage Duration(mins)", "Battery Consumptions(%)", "Data Consumption(in MB)",
                       "Times Opened"]
             fw.writerow(column)
-            print("File Successfully Created! ")
+            cprint("File Successfully Created! ", "green")
 
         else:
-            print("--File Was Not Created--")
+            cprint("--File Was Not Created--", "red")
 
-    elif fname not in file_list:
-        f = open(f"{app_data_path}/{fname}", 'w', newline="")
+    elif file_name not in file_list:
+        f = open(f"{app_data_path}/{file_name}", 'w', newline="")
         fw = csv.writer(f)
         column = ["Week Number", "Usage Duration(mins)", "Battery Consumptions(%)", "Data Consumption(in MB)",
                   "Times Opened"]
         fw.writerow(column)
-        print("File Successfully Created! ")
+        cprint("File Successfully Created! ", "green")
 
 
 def app_data_inserter(id1, app_name):
-    fname = f"User{id1}-{app_name}.csv"
-    f = open(f"{app_data_path}/{fname}", 'a', newline="")
+    file_name = f"User{id1}-{app_name}.csv"
+    f = open(f"{app_data_path}/{file_name}", 'a', newline="")
     fw = csv.writer(f)
     week_no = int(input("Enter The Week Number: "))
 
     try:
         usage_time = int(input(f"Enter the Usage Duration Of {app_name} for Week{week_no} (in minutes): "))
         battery_consumption = int(input(f"Enter the Battery Consumed by {app_name} for Week{week_no}: "))
-        data_consumption = int(input(f"Enter the Data Consumed by {app_name} for Week{week_no} (in Megabites): "))
+        data_consumption = int(input(f"Enter the Data Consumed by {app_name} for Week{week_no} (in Megabytes): "))
         times_opened = int(input(f"Enter How Many Times You Opened {app_name} in Week{week_no}: "))
         row = [f"Week{week_no}", usage_time, battery_consumption, data_consumption, times_opened]
         fw.writerow(row)
     except ValueError:
-        print("there was a value error")
+        cprint("there was a value error", 'red')
 
-    print(f"\n-----Data for Week{week_no} Has Been Successfully Added!-----\n\n")
+    cprint(f"\n-----Data for Week{week_no} Has Been Successfully Added!-----\n\n", 'green')
 
 
 def app_data_read(id1, app_name):
@@ -112,7 +194,7 @@ def app_data_read(id1, app_name):
         file_open = open(f"{app_data_path}/{file_name}", 'r')
         file_reader = csv.reader(file_open)
         opt = input(
-            "Select The Task\n1) Read Full Data\n2) Read Particular Week\nYour Option [1]or[2]")
+            "Select The Task\n1) Read Full Data\n2) Read Particular Week\nYour Option [1]or[2] : ")
         if opt == "1":
             print(Df(file_reader, ))
         elif opt == "2":
@@ -124,29 +206,80 @@ def app_data_read(id1, app_name):
                     print(Df(row, header))
                     found = True
             if found is False:
-                print("no such app found")
+                cprint("no such app found", 'red')
         else:
-            print("invalid option")
+            invalid_option()
     except FileNotFoundError:
         print("No Such App was Found")
 
 
+def sign_up():
+    while True:
+        details_ok = True
+        pwd = True
+        id1 = input("Enter your id : ")
+        name = input("Enter your name : ")
+        password = input("Enter your Password : ")
+
+        file1 = open(user_file_path, 'r')
+        reader = csv.reader(file1)
+
+        for row in reader:
+            if row[0] == id1:
+                cprint(f"ERROR IN (same id) ==> {row}", 'red')
+                details_ok = False
+            if row[1] == name:
+                cprint(f"ERROR IN (same Name) ==> {row}", 'red')
+                details_ok = False
+        file1.close()
+
+        if password == "":
+            details_ok = False
+            cprint("Password cannot be empty!!", 'red')
+
+        if password == " ":
+            details_ok = False
+            cprint("Password cannot be space!!", 'red')
+
+        if "," in password:
+            details_ok=False
+            cprint("You Cannot Use Comma In Password as it will mess up the database", 'red')
+
+        if len(password) < 8:
+            pwd = False
+
+        if details_ok is True:
+            write_user(id1, name, password)
+            cprint("New User Has Been Created Successfully !!", 'cyan')
+            if pwd is False:
+                cprint("We Recommend you Updating password to minimum 8 characters!!!", 'red')
+            break
+        if details_ok is False:
+            cprint("\nInvalid details provided! \n", 'red')
+            chance = input("Do you want leave sign-up page? [y] [n] : ")
+            if chance == "y":
+                break
+            else:
+                print("Invalid option! Continuing Sign-up process....")
+
+
 def login():
     while True:
-        f = open(user_file_path, 'r')
         id1 = input("Enter Your ID: ")
         password = input("Enter Your Password: ")
+        f = open(user_file_path, 'r')
         fr = csv.reader(f)
         auth = False
         for row in fr:
             if id1 in row and password in row:
-                print(f"You are Authorized with id {id1}!")
+                name = colored(f"=> {row[1]}", 'cyan')
+                print(f"You are Authorized with has {name}!")
                 auth = True
                 break
         if auth is True:
             break
         if auth is False:
-            print("You Credentials Are Wrong!\nTRY AGAIN")
+            cprint("You Credentials Are Wrong!\nTRY AGAIN", 'red')
             chance = input("Do you want leave login page? [y] [n] : ")
             if chance == "y":
                 break
@@ -167,13 +300,13 @@ def admin_login():
         for row in fr:
             if id1 in row and password in row:
                 power = row[2]
-                print(f"You are Authorized as Level{power} Admin User!")
+                print(f"You are Authorized as Level {power} Admin User!")
                 auth = True
                 break
         if auth is True:
             break
         if auth is False:
-            print("You Credentials Are Wrong!\nTRY AGAIN")
+            cprint("You Credentials Are Wrong!" , 'red')
             chance = input("Do you want leave login page? [y] [n] : ")
             if chance == "y":
                 break
@@ -182,41 +315,10 @@ def admin_login():
     return id1, power
 
 
-def sign_up():
-    while True:
-        details_ok = True
-        id1 = input("Enter your id : ")
-        name = input("Enter your name : ")
-        password = input("Enter your Password : ")
-
-        file1 = open(user_file_path, 'r')
-        reader = csv.reader(file1)
-
-        for row in reader:
-            if row[0] == id1:
-                print("ERROR IN (same id1) ==> ", row)
-                details_ok = False
-            if row[1] == name:
-                print("ERROR IN (same Name) ==> ", row)
-                details_ok = False
-        file1.close()
-
-        if details_ok is True:
-            write_user(id1, name, password)
-            break
-        if details_ok is False:
-            print("\nInvalid details provided! \n")
-            chance = input("Do you want leave sign-up page? [y] [n] : ")
-            if chance == "y":
-                break
-            else:
-                print("Invalid option! Continuing Sign-up process....")
-
-
 def add_admin():
     id1 = int(input("Enter Admin ID : "))
     pwd = input("Enter Admin Password : ")
-    power = "2" #by default 2
+    power = "2"  # by default 2
     lst = [id1, pwd, power]
 
     f = open(admin_file_path, "a", newline="")
@@ -241,21 +343,84 @@ def remove_admin():
         file = csv.writer(f)
         file.writerows(before)
 
+    print(f"Admin User With ID{id1} Has Been Removed! ")
 
-def update_user_data():
-    id1 = input("Enter id of the admin u wanna remove : ")
+
+def admin_password_update():
+    id1 = input("Enter id of the user u wanna update password : ")
     pwd = input("Enter the new password for the user : ")
     before = []
 
-    with open(admin_file_path, "r", newline="") as f:
+    with open(user_file_path, "r", newline="") as f:
         file = csv.reader(f)
 
         for row in file:
             if row[0] == id1:
                 row[2] = pwd
                 before.append(row)
+            else:
+                before.append(row)
 
     with open(user_file_path, "w", newline="") as f:
         file = csv.writer(f)
         file.writerows(before)
-        
+
+    cprint("User Password Updated Successfully", 'green')
+
+
+def user_password_update(id1):
+    auth = False
+    pass1 = True
+
+    old_password = input("Enter the old password : ")
+    pwd = input("Enter the new password : ")
+    confirm = input("Confirm the new password : ")
+
+    f = open(user_file_path, 'r')
+    fr = csv.reader(f)
+
+    for row in fr:
+        if id1 in row and old_password in row:
+            auth = True
+
+    if confirm != pwd:
+        cprint("Password did not match", 'red')
+        pass1 = False
+
+    if pwd == "":
+        pass1 = False
+        cprint("Password cannot be empty!!", 'red')
+
+    if pwd.startswith(" "):
+        pass1 = False
+        cprint("Password cannot be start with space!!", 'red')
+
+    if "," in pwd:
+        pass1 = False
+        cprint("You Cannot Use Comma In Password as it will mess up the database", 'red')
+
+    if len(pwd) < 8:
+        cprint("Password should be minimum 8 characters", 'red')
+        pass1 = False
+
+    if auth is True and pass1 is True:
+        before = []
+        with open(user_file_path, "r", newline="") as f:
+            file = csv.reader(f)
+
+            for row in file:
+                if row[0] == id1:
+                    row[2] = pwd
+                    before.append(row)
+                else:
+                    before.append(row)
+
+        with open(user_file_path, "w", newline="") as f:
+            file = csv.writer(f)
+            file.writerows(before)
+
+        cprint("User Password Updated Successfully", 'green')
+    elif not auth:
+        cprint("Did not enter proper old password!!", 'red')
+    else:
+        print("Password was not update due to above errors")
