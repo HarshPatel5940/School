2a02e359de18bb386a76af34aab27618b9ff72a8
harshpatel5940 <harshpatel5940>
2021-09-21 23:40:48 +0530

    learning js + `extra`

---
+++ experiments.py
@@ -0,0 +1,74 @@
+
+#======================= exp : 1
+# Program to calculate factorial of entered number
+num = int(input("Enter any number :"))
+fact = 1
+n = num # storing num in n for printing
+while num>1: # loop to iterate from n to 2
+    fact = fact * num
+    num-=1
+print("Factorial of ", n , " is :",fact)
+
+
+#======================= exp : 2
+# Program to input any number from user Check it is Prime number of not
+import math
+num = int(input("Enter any number :"))
+isPrime=True
+for i in range(2,int(math.sqrt(num))+1):
+    if num % i == 0:
+        isPrime=False
+
+if isPrime:
+    print(f"{num} Number is Prime")
+else:
+    print(f"{num} Number is not Prime")
+
+
+#======================= exp : 3  
+#Program to find sum of elements of list recursively
+def findSum(lst,num):
+    if num==0:
+        return 0
+    else:
+        return lst[num-1]+findSum(lst,num-1)
+
+mylist = [] #
+num = int(input("Enter how many number :"))
+for i in range(num):
+    n = int(input("Enter Element "+str(i+1)+":"))
+    mylist.append(n)
+sum = findSum(mylist,len(mylist))
+print("Sum of List items ",mylist, " is :",sum)
+
+
+#======================= exp : 4
+#Program to find 'n'th term of fibonacci series Fibonacci series : 0,1,1,2,3,5,8,13,21,34,55,89,...
+# nth term will be counted from 1 not 0
+def nthfiboterm(n):
+    if n<=1:
+        return n
+    else:
+        return (nthfiboterm(n-1)+nthfiboterm(n-2))
+
+num = int(input("Enter the 'n' term to find in fibonacci :"))
+term = nthfiboterm(num)
+print(num,"th term of fibonacci series is :",term)
+
+
+#======================= exp : 5
+#Program to find the occurence of any word in a string
+def countWord(str1,word):
+    s = str1.split()
+    count=0
+    for w in s:
+        if w==word:
+            count+=1
+    return count
+str1 = input("Enter any sentence :")
+word = input("Enter word to search in sentence :")
+count = countWord(str1,word)
+if count==0:
+    print("## Sorry! ",word," not present ")
+else:
+    print("## ",word," occurs ",count," times ")
--- testing.js
+++ testing.js
@@ -7,20 +7,49 @@ var number = 9;
 console.log(number);
 
 // part 1
-
-var a;              // unasignned variables
-var b = 2;         // asigned variable
+var a;          // unasignned variables
+var b = 2;     // asigned variable
 
 a = 5;
 console.log(a);
 
-//part 2
-
 console.log("All the mathematical operations are similar in js to python");
 
-console.log("")
+console.log("");
 
+// typeof key word
 
-// part 3 
-// Comparisons and Conditionals
+let number1 = 89;
+
+console.log(typeof number1);
+
+// Converting string to number
+
+const exp = "30";
+
+const test = parseInt(exp); /* you can also use //const test = +exp; to convert
+this happens when you use +exp Nan shortform means "Not a Number" */
+
+console.log(test)
+
+/*
+There Exsist three types
+    => Syntax Error
+    => Runtime Error
+    => Logical Error (MOST COMMON TYPE OF ERROR)
+*/
+
+console.log("Check Line 42 ONCE") // throw new Error("Any Thing You Wanna spam out")
+
+// Try catch Finally
+
+try {
+    console.log(1);
+    console.log(test);
+    console.log(2);
+} catch (error) {
+    console.log(error.message);           // .message here just prints the error in normal message rather than in red
+} finally {                              // finally is always executed no matter wats the problem
+    console.log("completed execution");
+}
 
